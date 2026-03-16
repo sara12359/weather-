f1ca95e97be39b7c3c2c8ddd3aff0263f586dde0
sara12359 <sarasalahmohamed97@gmail.com>
2026-03-16 21:55:12 +0200

    addition

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-class FavoriteCity(models.fields.Model):
+class FavoriteCity(models.Model):
     name = models.CharField(max_length=100, unique=True)
     added_at = models.DateTimeField(auto_now_add=True)
 
